2422dc2d533446210aad2b9d64695640bb5fe6c6
Aaqib Ali <75785706+Aaqibali1@users.noreply.github.com>
2023-07-11 21:13:42 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System.ComponentModel;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {/*
+        String songLyrics = "You say goodbye, and I say hello";
+        Console.WriteLine(songLyrics.Contains("goodbye"));
+        Console.WriteLine(songLyrics.Contains("greetings"));
+
+
+        int a = 18;
+        int b = 6;
+        int c = a + b;
+        Console.WriteLine(c);*/
+
+        /* int a = 5;
+         int b = 4;
+         int c = 2;
+         int d = a + b * c;
+         Console.WriteLine(d);*/
+
+        /* int a = 5; int b = 4; int c = 2;
+         int d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+         Console.WriteLine(d);*/
+
+
+        /* int a = 7;
+         int b = 4;
+         int c = 4;
+         int d = (a + b) / c;
+         int e = (a + b) / c;
+         Console.WriteLine($"quotient: {d}");
+         Console.WriteLine($"remainder {e}");*/
+
+
+        /* int max = int.MaxValue;
+         int min = int.MinValue;
+         Console.WriteLine($"The range of integers is {min} to {max}");
+
+         int what = max + 3;
+         Console.WriteLine($"An example of overflow: {what}");*/
+
+
+        /*double a = 5;
+        double b = 4;
+        double c = 2;
+        double d = (a + b) / c;
+        Console.WriteLine(d);
+
+        double e = 19;
+        double f = 23;
+        double g = 8;
+        double h = (e + f) / g;
+        Console.WriteLine(h);
+
+        double max = int.MaxValue;
+        double min = int.MinValue;
+        Console.WriteLine($"The range of double is {max} to {min}");
+
+
+        double third = 1.0 / 3.0;
+        Console.WriteLine(third);
+
+
+        decimal maxValue = decimal.MaxValue;
+        decimal minValue = decimal.MinValue;
+        Console.WriteLine($"The range of decimal type is {maxValue} to {minValue}");
+*/
+
+
+        double a = 1.0;
+        double b = 3.0;
+        Console.WriteLine(a/b);
+
+
+        decimal c = 1.0M;
+        decimal d = 3.0M;
+        Console.WriteLine(c/d);
+
+
+
+
+
+
+
+
+
+    }
+}
